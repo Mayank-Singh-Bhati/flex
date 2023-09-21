45481ed15ec8925b0f5c89432dcd30ed1ee173d8
Mayank-Singh-Bhati <121754832+Mayank-Singh-Bhati@users.noreply.github.com>
2023-09-22 02:08:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let imgPath = document.getElementById("menu");
+      let nav = document.getElementById("navitem");
+      const menuChange = () => {
+        if (imgPath.src.match("../img/menu.svg")) {
+          imgPath.src = "../img/close.svg";
+        } else {
+          imgPath.src = "../img/menu.svg";
+        }
+      };
+      const menuToggle = () => {
+        nav.classList.toggle("hidden");
+        nav.classList.toggle("flex");
+      }
