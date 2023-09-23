ce82e6dc307eaffdd2ed2f86fb041f4f24309876
Mayank-Singh-Bhati <121754832+Mayank-Singh-Bhati@users.noreply.github.com>
2023-09-23 18:54:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let imgPath = document.getElementById("menu");
       let nav = document.getElementById("navitem");
       const menuChange = () => {
-        if (imgPath.src.match("img/menu.svg")) {
-          imgPath.src = "img/close.svg";
+        if (imgPath.src.match("../img/menu.svg")) {
+          imgPath.src = "../img/close.svg";
+          document.body.style.overflowY = "hidden";
         } else {
-          imgPath.src = "img/menu.svg";
+          imgPath.src = "../img/menu.svg";
+          document.body.style.overflowY = "auto";
         }
       };
       const menuToggle = () => {
